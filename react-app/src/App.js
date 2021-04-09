90c82d377a5603205d02054556c62264b199e9c8
Jordan Gamble <jgamble482@gmail.com>
2021-04-09 02:18:13 -0400

    Added state property to app component, added click event handler

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,10 +12,20 @@ class App extends Component {
     ]
   }
 
+  switchNamesHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'Laura', age: 23},
+        {name: 'Kumar', age: 24},
+        {name: 'Jordan', age: 22}
+      ]
+    });
+  }
+
   render() { return (
     <div className="App">
       <h1>I'm a React App</h1>
-      <button>Click Me</button>
+      <button onClick={this.switchNamesHandler}>Switch Names</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>This one's me</Person>
