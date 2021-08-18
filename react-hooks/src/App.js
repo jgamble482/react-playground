04f1670a934c3d4fe547865a4ebc39f08248c7cd
Jordan Gamble <jgamble482@gmail.com>
2021-08-18 17:38:25 -0400

    Implemented useffect hook to check input field validity

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,16 +6,28 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+ 
+  
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    
     setIsLoggedIn(true);
+    localStorage.setItem('loginState', 'true');
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.setItem('loginState', 'false');
   };
+
+  useEffect(() => {
+    const loginState = localStorage.getItem('loginState');
+    if(loginState === 'true') {
+      setIsLoggedIn(true);
+    }
+  }, [])
 
   return (
     <React.Fragment>
