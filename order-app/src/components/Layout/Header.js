e96254a3b6876b8abb28bb54367c2ad3542a4a95
Jordan Gamble <jgamble482@gmail.com>
2021-08-23 00:08:29 -0400

    Created a new react app, added header component, and svg button

---
+++ order-app/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import { HeaderCartButton } from "./HeaderCartButton";
+
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Food Order App</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of food" />
+      </div>
+    </>
+  );
+};
+
+export { Header };
