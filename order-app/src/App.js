705773d507449f69b5f1d46718305c11900f1145
Jordan Gamble <jgamble482@gmail.com>
2021-08-23 23:33:17 -0400

    Added meals component and associated subcomponents

--- order-app/src/App.js
+++ order-app/src/App.js
@@ -1,11 +1,15 @@
-import { Header } from './components/Layout/Header';
-import './App.css';
+import { Header } from "./components/Layout/Header";
+import "./App.css";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-  <>
-    <Header/>
-  </>
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
   );
 }
 
