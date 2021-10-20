498a39868114cf6c4074114369fde91e884f6be7
Jordan Gamble <jgamble482@gmail.com>
2021-10-19 20:08:34 -0400

    Created proj, added a form with input validation

---
+++ form-project/src/components/SimpleInput.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+const SimpleInput = (props) => {
+  const [nameInput, setNameInput] = useState("");
+  const [nameInputTouched, setNameInputTouched] = useState(false);
+  const nameIsValid = nameInput.trim() !== "";
+  const nameInputInvalid = !nameIsValid && nameInputTouched;
+  const handleNameInputChange = (event) => {
+    setNameInput(event.target.value);
+
+  };
+
+  const inputBlurHandler = (event) => {
+    setNameInputTouched(true);
+  };
+  const handleFormSubmission = (event) => {
+    event.preventDefault();
+    setNameInputTouched(true);
+    if(!nameIsValid) {
+      return;
+    }
+
+    console.log(nameInput);
+    setNameInput("");
+    setNameInputTouched(false);
+  };
+  const nameInputClasses = nameInputInvalid
+    ? "form-control invalid"
+    : "form-control";
+  return (
+    <form onSubmit={handleFormSubmission}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          onBlur={inputBlurHandler}
+          onChange={handleNameInputChange}
+          value={nameInput}
+          type="text"
+          id="name"
+        />
+        {nameInputInvalid && (
+          <p className="error-text">Name Must Not Be Empty</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
